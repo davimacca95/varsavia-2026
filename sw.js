ccd227005b418c659a840fddb131926eb8dbf515
davimacca95 <davimacca95@users.noreply.github.com>
2026-04-09 22:10:29 +0200

    190 places: +72 restaurants, cafes, bars from Polish blogs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='v1775759904';
+var CACHE='v1775765429';
 var URLS=['/varsavia-2026/','/varsavia-2026/index.html'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
 self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
