f982715efc46f72d532cdf0c22ef90536d0a8a45
davimacca95 <davimacca95@users.noreply.github.com>
2026-04-09 22:21:54 +0200

    nuke old service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='v1775765939';
+var CACHE='v1775766114';
 var URLS=['/varsavia-2026/','/varsavia-2026/index.html'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
 self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
