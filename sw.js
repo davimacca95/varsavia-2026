fc55d864c967fd7369198791fb3b225e021dd086
davimacca95 <davimacca95@users.noreply.github.com>
2026-04-13 23:24:16 +0200

    consolidate subcats 24→12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='v1776115389';
+var CACHE='v1776115456';
 var URLS=['/varsavia-2026/','/varsavia-2026/index.html'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
 self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
